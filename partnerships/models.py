ff1c4ae582411a988377d0de75b7c5bdb038af16
Sergey <biggreen.rm@gmail.com>
2020-12-20 17:51:50 +0300

    fix many-to-many relation

--- partnerships/models.py
+++ partnerships/models.py
@@ -7,6 +7,6 @@ class Partnership(models.Model):
     Class which describes relations between different companies.
     """
 
-    company1 = models.ForeignKey(Company, related_name="company1")
-    company2 = models.ForeignKey(Company, related_name="company2")
+    company1 = models.ForeignKey(Company, related_name="company1", on_delete=models.CASCADE)
+    company2 = models.ForeignKey(Company, related_name="company2", on_delete=models.CASCADE)
     start_date = models.DateField()
