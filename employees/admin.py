8ab495f1d0ae64b8bea588cf86dcc240fe2b1e0d
Sergey <biggreen.rm@gmail.com>
2020-12-20 17:53:26 +0300

    try another type of registering ModelAdmin

--- employees/admin.py
+++ employees/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Employee
 
-@admin.register(Employee)
+
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("first_name", "second_name", "position",
                     "company", "sex", "salary", "date_of_birth",
                     "date_of_hiring")
+
+admin.site.register(Employee, EmployeeAdmin)
