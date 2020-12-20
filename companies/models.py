4628ec2bffe4a73be4b87ab496b520953c194199
Sergey <biggreen.rm@gmail.com>
2020-12-20 14:31:01 +0300

    describe Companies model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    """
+    Class that describes companies.
+    """
+
+    # setting up choices of company's area of activity
+    finance = "Finance"
+    marketing = "Marketing"
+    distribution = "Distribution"
+    production = "Production"
+    unknown = "Unknown"
+    AREA_CHOICES = (
+        (finance, "Finance"),
+        (marketing, "Marketing"),
+        (distribution, "Distribution"),
+        (production, "Production"),
+        (unknown, "Unknown"),
+    )
+
+    # description of the model
+    name = models.CharField(max_length=200, db_index=True)
+    country = models.CharField(max_length=200)
+    foundation = models.DateField()
+    area = models.CharField(max_length=200, choices=AREA_CHOICES, default=unknown)
