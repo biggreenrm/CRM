3f3673449b2511fbe3055fa14528a49c1e36dea1
Sergey <biggreen.rm@gmail.com>
2020-12-20 15:43:26 +0300

    Describe parthership model

--- partnerships/models.py
+++ partnerships/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from companies.models import Company
 
-# Create your models here.
+
+class Partnership(models.Model):
+    """
+    Class which describes relations between different companies.
+    """
+
+    company1 = models.ForeignKey(Company, related_name="company1")
+    company2 = models.ForeignKey(Company, related_name="company2")
+    start_date = models.DateField()
