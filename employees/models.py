287ad77f8e4e3852205d1ad193b679e72c1c73e2
Sergey <biggreen.rm@gmail.com>
2020-12-20 14:46:11 +0300

    Describe employees model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from companies.models import Company
 
-# Create your models here.
+
+class Employee(models.Model):
+    """
+    Class which describes employees working on different companies.
+    """
+
+    # describing employee's sex
+    male = "Male"
+    female = "Female"
+    SEX_CHOICES = (
+        (male, "Male"),
+        (female, "Female"),
+        (secret, "Secret"),
+    )
+
+    # describing employee's model
+    first_name = models.CharField(max_length=255, blank=False)
+    second_name = models.CharField(max_length=255, blank=False)
+    position = models.CharField(max_length=255, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, blank=True)
+    sex = models.CharField(max_length=255, choices=SEX_CHOICES, default=secret)
+    salary = models.IntegerField(blank=True)
+    date_of_birth = models.DateField(blank=True)
+    date_of_hiring = models.DateField(blank=True)
