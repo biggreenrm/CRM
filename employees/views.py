6b46052c9b5e335304f310f77488cb873e9878a8
Sergey <biggreen.rm@gmail.com>
2020-12-22 19:51:46 +0300

    Create ViewSet for Employees model

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,19 @@
+# First-party
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+# Django
 from django.shortcuts import render
 
-# Create your views here.
+# Third-party
+from rest_framework import viewsets
+from rest_framework import permissions
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    API эндпоинт, который позволяет компаниям быть отображенными, или изменять их.
+    """
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticated]
