9fc2ea91fa24a000884be150d07ddbc5c4b29033
Sergey <biggreen.rm@gmail.com>
2020-12-22 20:03:27 +0300

    Register Partnership URLs

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,6 +1,7 @@
 # First-party
 from companies import views as companies_views
 from employees import views as employees_views
+from partnerships import views as partnerships_views
 
 # Django
 from django.contrib import admin
@@ -17,10 +18,14 @@ router_companies.register('companies', companies_views.CompanyViewSet)
 router_employees = routers.DefaultRouter()
 router_employees.register('employees', employees_views.EmployeeViewSet)
 
+router_partnerships = routers.DefaultRouter()
+router_partnerships.register('partnerships', partnerships_views.PartnershipViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), # логирование в "browsable API"
     path('', include(router_companies.urls)), # register urls for companies
     path('', include(router_employees.urls)), # register urls for employees
+    path('', include(router_partnerships.urls)), # register urls for partnerships
 ]
