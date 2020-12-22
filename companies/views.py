e98140053138ee30b62a2d4b4dbff5a5fb1aa2f5
Sergey <biggreen.rm@gmail.com>
2020-12-22 18:58:08 +0300

    Create ViewSet for Company model

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,19 @@
+# First-party
+from .models import Company
+from .serializers import CompanySerializer
+
+# Django
 from django.shortcuts import render
 
-# Create your views here.
+# Third-party
+from rest_framework import viewsets
+from rest_framework import permissions
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    """
+    API эндпоинт, который позволяет компаниям быть отображенными, или изменять их.
+    """
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated] # как это работает?
