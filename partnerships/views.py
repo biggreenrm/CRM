889f499e56f7d7f02cf3b35662591b7bc00f61eb
Sergey <biggreen.rm@gmail.com>
2020-12-22 20:03:08 +0300

    Create ViewSet for Partnership Model

--- partnerships/views.py
+++ partnerships/views.py
@@ -1,3 +1,19 @@
+# First-party
+from .models import Partnership
+from .serializers import PartnershipSerializer
+
+# Django
 from django.shortcuts import render
 
-# Create your views here.
+# Third-party
+from rest_framework import viewsets
+from rest_framework import permissions
+
+
+class PartnershipViewSet(viewsets.ModelViewSet):
+    """
+    API эндпоинт, который позволяет партнёрствам быть отображенными или изменёнными.
+    """
+    queryset = Partnership.objects.all()
+    serializer_class = PartnershipSerializer
+    permission_classes = [permissions.IsAuthenticated]
