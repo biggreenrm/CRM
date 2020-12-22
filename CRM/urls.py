f8d6d1cd4c70b42f576634f003ad19efdc78f472
Sergey <biggreen.rm@gmail.com>
2020-12-22 19:13:58 +0300

    Create URLs for Companies API using the simpliest way - DefaultRouter

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,7 +1,21 @@
+# First-party
+from companies import views
+
+# Django
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# Third-party
+from rest_framework import routers
+
+# URLs генерируются автоматически через router, это "магия" DRF
+# Это самый упрощенный случай, можно использовать и более гибкий подход
+router = routers.DefaultRouter()
+router.register('companies', views.CompanyViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')), # логирование в "browsable API"
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), # логирование в "browsable API"
+    path('', include(router.urls)),
 ]
